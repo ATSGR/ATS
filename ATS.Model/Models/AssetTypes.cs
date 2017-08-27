68d0c6aace2498d9033f86e8c0fda4019db20945
nasir212 <nasir05988@gmail.com>
2017-08-27 16:59:55 +0600

    Asset type model is created

---
+++ ATS.Model/Models/AssetTypes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATS.Model.Models
+{
+    class AssetTypes
+    {
+        public long Id { get; set; }
+        [Display(Name = "Asset Name")]
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+        [Display(Name = "Short Name")]
+        [Required(ErrorMessage = "Short name is required")]
+        public string ShortName { get; set; }
+        [Display(Name = "Code")]
+        public string Code { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string ModefiedBy { get; set; }
+        public DateTime ModefiedOn { get; set; }
+    }
+}
