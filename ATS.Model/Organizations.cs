7678348b5a94a3b6452ea7590529946119192351
nasir212 <nasir05988@gmail.com>
2017-08-27 15:47:20 +0600

    Entity framework reference added and Organization model created

---
+++ ATS.Model/Organizations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATS.Model
+{
+    class Organizations
+    {
+
+        public long Id { get; set; }
+        [Display(Name = "Organization Name")]
+        [Required(ErrorMessage = "Organization name is required.")]
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string ModefiedBy { get; set; }
+        public DateTime ModefiedOn { get; set; }
+
+
+
+    }
+}
