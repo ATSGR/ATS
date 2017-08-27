7f0876608cc23e3ccd1f93bdf0b0ced21791149c
nasir212 <nasir05988@gmail.com>
2017-08-27 15:51:25 +0600

    model folder added in ats.model class

--- ATS.Model/Models/Organizations.cs
+++ ATS.Model/Models/Organizations.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ATS.Model
+namespace ATS.Model.Models
 {
     class Organizations
     {
