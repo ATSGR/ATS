f5e84b072bdc23779f9d838f690d7501794452c4
nasir212 <nasir05988@gmail.com>
2017-09-07 11:07:52 +0600

    Asset group model created

---
+++ ATS.Model/Models/AssetGroups.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATS.Model.Models
+{
+    class AssetGroups
+    {
+        public long Id { get; set; }
+        [Display(Name = "Asset Type")]
+        public long AssetTypesId { get; set; }//FK from asset type table
+        [Required(ErrorMessage = "Group name is required")]
+        public string Name { get; set; }
+        [Display(Name = "Short Name")]
+        [Required(ErrorMessage = "Short Name is required")]
+        public string ShortName { get; set; }
+        public string Code { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string ModefiedBy { get; set; }
+        public DateTime ModefiedOn { get; set; }
+    }
+}
